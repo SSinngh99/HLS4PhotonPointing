2dcd49c85b6f435726ea8bd9fda4e145d4e2abaf
sasingh <sahibjeet.singh@cern.ch>
2026-03-18 13:44:33 +0100

    Old commit forgot to add - Naoki's eta indexing scheme

--- PPZCalc/PPZCalc_test.cpp
+++ PPZCalc/PPZCalc_test.cpp
@@ -2,9 +2,43 @@
 #include <cstdint>
 #include <cstdlib>
 #include "PPZCalc.h"
+#include "PPZCalc_test.h"
 
 
+int GetL1EtaIdx(float eta){
+  int max_eta_ind;
+  if (eta == -999){return -999;}
+  if(eta < -2.4){
+    max_eta_ind = (int)((eta + 2.5 - 0.1/8.) / (0.1/4.));
+}else if(eta < -2.0){
+    max_eta_ind = (int)((eta + 2.4 - 0.1/32.) / (0.1/16.)) + 4;
+}else if(eta < -1.8){
+    max_eta_ind = (int)((eta + 2.0 - 0.1/48.) / (0.1/24.)) + 68;
+}else if(eta < -1.5){
+    max_eta_ind = (int)((eta + 1.8 - 0.1/64.) / (0.1/32.)) + 116;
+}else if(eta < -1.4){
+    max_eta_ind = (int)((eta + 1.5 - 0.1/8.) / (0.1/4.)) + 212;
+}else if(eta < 1.4){
+    max_eta_ind = (int)((eta + 1.4 - 0.1/64.) / (0.1/32.)) + 216;
+}else if(eta < 1.5){
+    max_eta_ind = (int)((eta - 1.4 - 0.1/8.) / (0.1/4.)) + 1112;
+}else if(eta < 1.8){
+    max_eta_ind = (int)((eta - 1.5 - 0.1/64.) / (0.1/32.)) + 1116;
+}else if(eta < 2.0){
+    max_eta_ind = (int)((eta - 1.8 - 0.1/48.) / (0.1/24.)) + 1212;
+}else if(eta < 2.4){
+    max_eta_ind = (int)((eta - 2.0 - 0.1/32.) / (0.1/16.)) + 1260;
+}else{
+    max_eta_ind = (int)((eta - 2.4 - 0.1/8.) / (0.1/4.)) + 1324;
+}
 
+  return max_eta_ind;
+}
+
+int GetL2EtaIdx(float eta){
+  if (eta == -999){return -999;}
+  return int((eta + 2.5 - 0.1/8.) / (0.1/4.));
+}
 
 
 int main(){
@@ -18,8 +52,17 @@ hls::stream<fZ> OutputStream_Test;
 std::vector<float> InputVectorEtaL1 = {-2.099481, -1.181972, -999, 0.221235, -2.082226};
 std::vector<float> InputVectorEtaL2 = {-2.098641, -1.191262, 0.112489, 0.202920, -2.060124};
 
-std::vector<int> InputEtaL1Idx = {0, 1, -9999, 2, 3};
-std::vector<int> InputEtaL2Idx = {4, 5, 6, 7, 8};
+std::vector<int> InputEtaL1Idx, InputEtaL2Idx;
+
+for (int i = 0; i < InputVectorEtaL1.size(); ++i){
+  int Eta1Idx = GetL1EtaIdx(InputVectorEtaL1.at(i));
+  int Eta2Idx = GetL2EtaIdx(InputVectorEtaL2.at(i));
+  InputEtaL1Idx.push_back(Eta1Idx);
+  InputEtaL2Idx.push_back(Eta2Idx);
+  std::cout << "Eta1 = " << InputVectorEtaL1.at(i) << "-->" << Eta1Idx << std::endl;
+  std::cout << "Eta2 = " << InputVectorEtaL2.at(i) << "-->" << Eta2Idx << "\n" << std::endl;
+}
+
 
 
 for (int i = InputVectorEtaL1.size(); i < NMaxClusters; ++i){
@@ -40,7 +83,7 @@ for (int i = InputVectorEtaL1.size(); i < NMaxClusters; ++i){
 
 
 for (int i = 0; i < NMaxClusters; ++i) {
-    std::cout << "eta1[" << i << "] = " << InputEtaL1Idx[i] << std::endl;
+    std::cout << "eta1Idx[" << i << "] = " << InputEtaL1Idx[i] << std::endl;
     InputStreamEtaL1.write(InputEtaL1Idx[i]);
     InputStreamEtaL2.write(InputEtaL2Idx[i]);
   }
---
+++ PPZCalc/PPZCalc_test.h
@@ -0,0 +1,7 @@
+#ifndef PPZCALCTEST_H
+#define PPZCALCTEST_H
+
+int GetL1EtaIdx(float eta);
+int GetL2EtaIdx(float eta);
+
+#endif // PPZCALCTEST_H
