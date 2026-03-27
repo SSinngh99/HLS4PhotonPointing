10ee54ef733336343b9db22ffff0d449773670fc
root <root@SahibsSC.localdomain>
2026-03-27 14:01:09 +0100

    Added Naokis eta map here as well

--- tower_barycenter_calc/BarycenterCalc_test.cpp
+++ tower_barycenter_calc/BarycenterCalc_test.cpp
@@ -4,40 +4,40 @@
 #include "BarycenterCalc.h"
 
 
-// int GetL1EtaIdx(float eta){
-//   int max_eta_ind;
-//   if (eta == -999){return -999;}
-//   if(eta < -2.4){
-//     max_eta_ind = (int)((eta + 2.5 - 0.1/8.) / (0.1/4.));
-// }else if(eta < -2.0){
-//     max_eta_ind = (int)((eta + 2.4 - 0.1/32.) / (0.1/16.)) + 4;
-// }else if(eta < -1.8){
-//     max_eta_ind = (int)((eta + 2.0 - 0.1/48.) / (0.1/24.)) + 68;
-// }else if(eta < -1.5){
-//     max_eta_ind = (int)((eta + 1.8 - 0.1/64.) / (0.1/32.)) + 116;
-// }else if(eta < -1.4){
-//     max_eta_ind = (int)((eta + 1.5 - 0.1/8.) / (0.1/4.)) + 212;
-// }else if(eta < 1.4){
-//     max_eta_ind = (int)((eta + 1.4 - 0.1/64.) / (0.1/32.)) + 216;
-// }else if(eta < 1.5){
-//     max_eta_ind = (int)((eta - 1.4 - 0.1/8.) / (0.1/4.)) + 1112;
-// }else if(eta < 1.8){
-//     max_eta_ind = (int)((eta - 1.5 - 0.1/64.) / (0.1/32.)) + 1116;
-// }else if(eta < 2.0){
-//     max_eta_ind = (int)((eta - 1.8 - 0.1/48.) / (0.1/24.)) + 1212;
-// }else if(eta < 2.4){
-//     max_eta_ind = (int)((eta - 2.0 - 0.1/32.) / (0.1/16.)) + 1260;
-// }else{
-//     max_eta_ind = (int)((eta - 2.4 - 0.1/8.) / (0.1/4.)) + 1324;
-// }
+int GetL1EtaIdx(float eta){
+  int max_eta_ind;
+  if (eta == -999){return -999;}
+  if(eta < -2.4){
+    max_eta_ind = (int)((eta + 2.5 - 0.1/8.) / (0.1/4.));
+}else if(eta < -2.0){
+    max_eta_ind = (int)((eta + 2.4 - 0.1/32.) / (0.1/16.)) + 4;
+}else if(eta < -1.8){
+    max_eta_ind = (int)((eta + 2.0 - 0.1/48.) / (0.1/24.)) + 68;
+}else if(eta < -1.5){
+    max_eta_ind = (int)((eta + 1.8 - 0.1/64.) / (0.1/32.)) + 116;
+}else if(eta < -1.4){
+    max_eta_ind = (int)((eta + 1.5 - 0.1/8.) / (0.1/4.)) + 212;
+}else if(eta < 1.4){
+    max_eta_ind = (int)((eta + 1.4 - 0.1/64.) / (0.1/32.)) + 216;
+}else if(eta < 1.5){
+    max_eta_ind = (int)((eta - 1.4 - 0.1/8.) / (0.1/4.)) + 1112;
+}else if(eta < 1.8){
+    max_eta_ind = (int)((eta - 1.5 - 0.1/64.) / (0.1/32.)) + 1116;
+}else if(eta < 2.0){
+    max_eta_ind = (int)((eta - 1.8 - 0.1/48.) / (0.1/24.)) + 1212;
+}else if(eta < 2.4){
+    max_eta_ind = (int)((eta - 2.0 - 0.1/32.) / (0.1/16.)) + 1260;
+}else{
+    max_eta_ind = (int)((eta - 2.4 - 0.1/8.) / (0.1/4.)) + 1324;
+}
 
-//   return max_eta_ind;
-// }
+  return max_eta_ind;
+}
 
-// int GetL2EtaIdx(float eta){
-//   if (eta == -999){return -999;}
-//   return int((eta + 2.5 - 0.1/8.) / (0.1/4.));
-// }
+int GetL2EtaIdx(float eta){
+  if (eta == -999){return -999;}
+  return int((eta + 2.5 - 0.1/8.) / (0.1/4.));
+}
 
 
 int main(){
@@ -50,14 +50,20 @@ hls::stream<bool> InputStreamBitMask;
 hls::stream<fBC> OutputStream_Test;
 
 std::vector<float> InputStreamEt_Vec =     {0.2, 10, 14, 28, 9, 5, 8};
-std::vector<int> InputStreamEtaIdx_Vec =   {1,    4,  3,  2, 5, 7, 9};
+std::vector<float> InputVectorEta = {-2.48828125, -2.46484375, -2.390625, -2.36328125, -2.33984375, -2.3125, -2.23828125}; // idx = (0, 1, 4, 5, 6, 7, 10)
 std::vector<bool> InputStreamBitMask_Vec = {1,    0,  1,  0, 0, 0, 1};
+std::vector<int> InputStreamEtaIdx_Vec;
 
 // Fill up bitmask to 64 to not consider stuff beyond the things written above
 for (int i = InputStreamBitMask_Vec.size(); i < NCells; ++i){
   InputStreamBitMask_Vec.push_back(0);
 }
 
+// Convert eta to etaIdx
+for (int i = 0; i < InputVectorEta.size(); ++i){
+  int EtaIdx2 = GetL2EtaIdx(InputVectorEta.at(i)); // Using L2 mapping
+  InputStreamEtaIdx_Vec.push_back(EtaIdx2);
+}
 
 for (int i = 0; i < NCells; ++i) {
     InputStreamEt.write(static_cast<fET>(InputStreamEt_Vec[i]));
